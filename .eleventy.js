546ad7d979e1c26ea8dc6feb12668bdd25e822e1
Erik Müller <emueller@newstore.com>
2024-08-05 10:55:55 +0200

    remove tmp association

--- .eleventy.js
+++ .eleventy.js
@@ -26,35 +26,6 @@ const getTopics = () => {
 module.exports = (eleventyConfig) => {
   const markdownLib = markdownIt({ html: true }).use(markdownItAttrs)
 
-  fs.mkdirSync('_site/.well-known')
-  fs.writeFileSync(
-    '_site/.well-known/apple-app-site-association',
-    JSON.stringify({
-      applinks: {
-        details: [
-          {
-            appIDs: [
-              '49BJ58VWBC.com.newstore.associate-one',
-              'KN956PWX39.com.newstore.associate-one',
-            ],
-            components: [
-              {
-                '#': 'no_universal_links',
-                exclude: true,
-                comment:
-                  'Matches any URL with a fragment that equals no_universal_links and instructs the system not to open it as a universal link.',
-              },
-              {
-                '/': '/nfc/*',
-                comment: 'Matches any URL with a path that starts with /nfc/.',
-              },
-            ],
-          },
-        ],
-      },
-    })
-  )
-
   eleventyConfig.setLibrary('md', markdownLib)
   eleventyConfig.addLiquidShortcode('static', webpackAsset)
   eleventyConfig.addGlobalData('topics', getTopics)
